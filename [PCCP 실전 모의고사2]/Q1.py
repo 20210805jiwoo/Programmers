10870146b370d916a76628b987fc781842ea3c92
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-02-02 17:45:08 +0900

    Update Q1.py

--- [PCCP 실전 모의고사2]/Q1.py
+++ [PCCP 실전 모의고사2]/Q1.py
@@ -1,3 +1,23 @@
 def solution(input_string):
     answer = ''
+    
+    str_list = [input_string[0]]
+    for i in input_string:
+        if i != str_list[-1]:       # 한 문자가 연속적으로 나타난 경우 제거
+            str_list.append(i)
+    
+    ans = set(str_list)
+    ans = sorted(ans)       # ans 딕셔너리를 알파벳 순으로 정렬함
+        
+    for i in ans:
+        cnt = 0
+        for j in str_list:
+            if i == j:
+                cnt += 1
+        if cnt > 1:
+            answer += i
+    
+    if answer == '':
+        answer += 'N'
+    
     return answer
