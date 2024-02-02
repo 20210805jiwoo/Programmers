b367c97cf427a370e23e10b654355d2b3b60ac41
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-02-02 18:04:44 +0900

    Update Q1(외톨이 알파벳).py

--- [PCCP 실전 모의고사1]/Q1(외톨이 알파벳).py
+++ [PCCP 실전 모의고사1]/Q1(외톨이 알파벳).py
@@ -1,23 +1,22 @@
 def solution(input_string):
     answer = ''
-    
-    str_list = [input_string[0]]
-    for i in input_string:
-        if i != str_list[-1]:       # 한 문자가 연속적으로 나타난 경우 제거
+
+    str_list = [input_string[0]] # 리스트 생성, 첫 번째 문자 리스트에 추가
+    for i in input_string: # 문자열 순회하면서
+        if i != str_list[-1]: # 한 문자가 연속적으로 나타난 경우 제거
             str_list.append(i)
     
-    ans = set(str_list)
-    ans = sorted(ans)       # ans 딕셔너리를 알파벳 순으로 정렬함
-        
+    ans = set(str_list) # 중복 제거 위해 set 자료형으로 변환
+    ans = sorted(ans) # 알파벳 순으로 정렬
+
     for i in ans:
         cnt = 0
         for j in str_list:
-            if i == j:
+            if i==j:
                 cnt += 1
         if cnt > 1:
             answer += i
-    
     if answer == '':
         answer += 'N'
-    
+
     return answer
