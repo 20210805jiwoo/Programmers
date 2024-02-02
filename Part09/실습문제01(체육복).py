c622b9c46beeb6a2e0ccfa1b04f021b2d5557c0a
Lee jiwoo <102974424+20210805jiwoo@users.noreply.github.com>
2024-02-02 15:09:56 +0900

    Update 실습문제01(체육복).py

--- Part09/실습문제01(체육복).py
+++ Part09/실습문제01(체육복).py
@@ -1,3 +1,4 @@
+# 방법 1
 def solution(n, lost, reserve):
     u = [1] * (n + 2)
     for i in reserve:
@@ -10,3 +11,15 @@ def solution(n, lost, reserve):
         elif u[i] == 2 and u[i+1] == 0:
             u[i:i+2] = [1, 1]
     return len([x for x in u[1:-1] if x >0])
+
+# 방법 2
+def solution(n, lost, reserve):
+    s = set(lost) & set(reserve) # 가져왔는데 잃어버린
+    l = set(lost) - s
+    r = set(reserve) - s
+    for x in sorted(r):
+        if x - l in l:
+            l.remove(x - 1)
+        elif x + 1 in l:
+            l.remove(x + 1)
+    return n - len(l)
